829011d25fbc44021de8d7a8f3a15a90678a47b3
squach28 <squach3443@gmail.com>
2023-06-08 21:15:53 -0700

    basic login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 const Login = () => {
 
@@ -10,12 +11,19 @@ const Login = () => {
     }
   return (
     <div className={styles.container}>
-        <h1>Login</h1>
+        <h1>Welcome Back!</h1>
+        <h2>Ready for more math?</h2>
         <form className={styles.form} onSubmit={handleSubmit}>
             <input type="text" placeholder="Email" className={styles.input} />
             <input type="password" placeholder="Password" className={styles.input} />
             <button className={styles.loginButton}>Login</button>
         </form>
+        <Link href="/register">Haven&apos;t mathed with us yet? Register here</Link>
+
+        <button className={styles.loginButton}>Google Login</button>
+        <button className={styles.loginButton}>Apple Login</button>
+        <button className={styles.loginButton}>Twitter Login</button>
+
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
+"use client"
+
 import React from 'react'
 import styles from './Navbar.module.css'
 import Link from 'next/link'
 
 const Navbar = () => {
+
   return (
     <div className={styles.container}>
         <Link href="/" className={styles.header}><h1>Quick Mafs</h1></Link>
